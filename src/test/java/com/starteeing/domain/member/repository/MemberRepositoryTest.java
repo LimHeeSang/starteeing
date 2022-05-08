168f65b813b8d531e8a4d600d742543079329764
heesang <dlagmltkd12@naver.com>
2022-05-09 02:47:26 +0900

    test : MemberRepository 테스트 작성

---
+++ src/test/java/com/starteeing/domain/member/repository/MemberRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.starteeing.domain.member.repository;
+
+import com.starteeing.domain.member.entity.MemberRole;
+import com.starteeing.domain.member.entity.SchoolInfo;
+import com.starteeing.domain.member.entity.UserMember;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private UserMemberRepository userMemberRepository;
+
+    private UserMember userMember;
+
+    @BeforeEach
+    void setUp() {
+        SchoolInfo schoolInfo = SchoolInfo.builder()
+                .school("순천향대")
+                .department("정보보호학과")
+                .uniqSchoolNumber("12345678")
+                .build();
+
+        userMember = UserMember.builder()
+                .name("홍길동")
+                .email("abc@naver.com")
+                .memberRole(MemberRole.ROLE_USER)
+                .nickName("길동이")
+                .birthOfDate(LocalDate.of(1998, 9, 4))
+                .phoneNumber("010-8543-0619")
+                .mbti("estj")
+                .schoolInfo(schoolInfo)
+                .build();
+
+        userMemberRepository.save(userMember);
+    }
+
+    @Test
+    void existsByEmail() {
+        boolean result = memberRepository.existsByEmail("abc@naver.com");
+        assertThat(result).isTrue();
+    }
+}
