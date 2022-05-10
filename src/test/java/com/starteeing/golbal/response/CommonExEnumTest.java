aba00754e3ee049611b7372074f41c7e43d8ac9a
heesang <dlagmltkd12@naver.com>
2022-05-11 05:16:06 +0900

    test : CommonExEnumTest 테스트 추가

--- src/test/java/com/starteeing/golbal/response/CommonExEnumTest.java
+++ src/test/java/com/starteeing/golbal/response/CommonExEnumTest.java
@@ -8,15 +8,29 @@ import static org.assertj.core.api.Assertions.*;
 class CommonExEnumTest {
 
     @Test
-    void create() {
+    void success() {
         CommonExEnum success = CommonExEnum.SUCCESS;
-        CommonExEnum fail = CommonExEnum.FAIL;
         CommonExEnum success2 = CommonExEnum.valueOf("SUCCESS");
 
         assertThat(success.getCode()).isEqualTo("E0001");
         assertThat(success.getMessage()).isEqualTo("정상 요청입니다.");
+
+        assertThat(success2).isEqualTo(CommonExEnum.SUCCESS);
+    }
+
+    @Test
+    void fail() {
+        CommonExEnum fail = CommonExEnum.FAIL;
+
         assertThat(fail.getCode()).isEqualTo("E0002");
         assertThat(fail.getMessage()).isEqualTo("잘못된 요청입니다.");
-        assertThat(success2).isEqualTo(CommonExEnum.SUCCESS);
+    }
+
+    @Test
+    void invalid_binding_value() {
+        CommonExEnum bingingValue = CommonExEnum.INVALID_BINGING_VALUE;
+
+        assertThat(bingingValue.getCode()).isEqualTo("E0003");
+        assertThat(bingingValue.getMessage()).isEqualTo("입력값 중 바인딩이 실패하였습니다.");
     }
 }
