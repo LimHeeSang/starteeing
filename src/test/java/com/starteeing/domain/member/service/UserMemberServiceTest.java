91ee6e0510d0161889885227a327b0ef7ee7faa5
heesang <dlagmltkd12@naver.com>
2022-05-12 05:15:12 +0900

    refactor : UserMemberServiceTest repository의존성 끊기

--- src/test/java/com/starteeing/domain/member/service/UserMemberServiceTest.java
+++ src/test/java/com/starteeing/domain/member/service/UserMemberServiceTest.java
@@ -2,36 +2,51 @@ package com.starteeing.domain.member.service;
 
 import com.starteeing.domain.member.dto.UserMemberRequestDto;
 import com.starteeing.domain.member.entity.MemberRole;
+import com.starteeing.domain.member.exception.ExistMemberException;
+import com.starteeing.domain.member.repository.MemberRepository;
 import com.starteeing.domain.member.repository.UserMemberRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class UserMemberServiceTest {
 
-    @Autowired
-    UserMemberService userMemberService;
-    @Autowired
+    @Mock
     UserMemberRepository userMemberRepository;
+    @Mock
+    MemberRepository memberRepository;
+    @InjectMocks
+    UserMemberService userMemberService;
 
     @Test
     void 회원가입() {
+        when(userMemberRepository.save(any())).thenReturn(createUserMemberRequestDto().toEntity());
+
         UserMemberRequestDto memberRequestDto = createUserMemberRequestDto();
 
-        Long savedId = userMemberService.memberJoin(memberRequestDto);
+        userMemberService.memberJoin(memberRequestDto);
+    }
 
-        assertThat(savedId).isEqualTo(userMemberRepository.findById(savedId).get().getId());
+    @Test
+    void 중복회원_회원가입_예외() {
+        when(memberRepository.existsByEmail("abc@naver.com")).thenReturn(true);
+
+        assertThatThrownBy(
+                () -> userMemberService.memberJoin(createUserMemberRequestDto())
+        ).isInstanceOf(ExistMemberException.class);
     }
 
     private UserMemberRequestDto createUserMemberRequestDto() {
-        UserMemberRequestDto memberRequestDto = UserMemberRequestDto.builder()
+        return UserMemberRequestDto.builder()
                 .name("홍길동")
                 .email("abc@naver.com")
                 .memberRole(MemberRole.ROLE_USER)
@@ -43,6 +58,5 @@ class UserMemberServiceTest {
                 .department("정보보호학과")
                 .uniqSchoolNumber("12345678")
                 .build();
-        return memberRequestDto;
     }
 }
