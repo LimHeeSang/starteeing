ae80a145946ed95c5e4d9dcfd98012da95efaa1d
heesang <dlagmltkd12@naver.com>
2022-05-13 03:19:42 +0900

    refactor : FriendServiceTest repository 의존성 제거

--- src/test/java/com/starteeing/domain/freinds/service/FriendServiceTest.java
+++ src/test/java/com/starteeing/domain/freinds/service/FriendServiceTest.java
@@ -8,33 +8,39 @@ import com.starteeing.domain.member.entity.MemberRole;
 import com.starteeing.domain.member.entity.SchoolInfo;
 import com.starteeing.domain.member.entity.UserMember;
 import com.starteeing.domain.member.repository.UserMemberRepository;
-import org.apache.tomcat.jni.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
-@SpringBootTest
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class FriendServiceTest {
-    @Autowired
+    @Mock
     UserMemberRepository userMemberRepository;
-    @Autowired
+    @Mock
     FriendRepository friendRepository;
-    @Autowired
+    @InjectMocks
     FriendService friendService;
 
     UserMember member1;
     UserMember member2;
     UserMember member3;
+
+    Long userMemberId1;
+    Long userMemberId2;
+    Long userMemberId3;
 
     @BeforeEach
     void setUp() {
@@ -42,18 +48,16 @@ class FriendServiceTest {
         member2 = createUserMember("bbb@naver.com", "userB", "010-1234-0001", "12340001");
         member3 = createUserMember("ccc@naver.com", "userC", "010-1234-0002", "12340002");
 
-        userMemberRepository.save(member1);
-        userMemberRepository.save(member2);
-        userMemberRepository.save(member3);
+        userMemberId1 = 1L;
+        userMemberId2 = 2L;
+        userMemberId3 = 3L;
+
+        ReflectionTestUtils.setField(member1, "id", userMemberId1);
+        ReflectionTestUtils.setField(member2, "id", userMemberId2);
+        ReflectionTestUtils.setField(member3, "id", userMemberId3);
     }
 
-    public UserMember createUserMember(String email, String nickName, String phoneNumber, String schoolNumber) {
-        SchoolInfo schoolInfo = SchoolInfo.builder()
-                .school("순천향대")
-                .department("정보보호학과")
-                .uniqSchoolNumber(schoolNumber)
-                .build();
-
+    private UserMember createUserMember(String email, String nickName, String phoneNumber, String schoolNumber) {
         return UserMember.builder()
                 .name("홍길동")
                 .email(email)
@@ -63,16 +67,37 @@ class FriendServiceTest {
                 .phoneNumber(phoneNumber)
                 .mbti("estj")
                 .temperature(37.5D)
-                .schoolInfo(schoolInfo)
+                .schoolInfo(createSchoolInfo(schoolNumber))
                 .build();
+    }
+
+    private SchoolInfo createSchoolInfo(String schoolNumber) {
+        SchoolInfo schoolInfo = SchoolInfo.builder()
+                .school("순천향대")
+                .department("정보보호학과")
+                .uniqSchoolNumber(schoolNumber)
+                .build();
+        return schoolInfo;
+    }
+
+    private void addFriend(UserMember toMember, UserMember fromMember, FriendStatus friendStatus) {
+        toMember.getFriends().add(Friend.builder()
+                .userMember(fromMember)
+                .friendId(fromMember.getId())
+                .friendsStatus(friendStatus)
+                .build());
     }
 
     @Test
     void 친구추가_처음요청() {
-        friendService.requestFriend(member1.getId(), "userB");
+        given(userMemberRepository.findById(userMemberId1)).willReturn(
+                Optional.ofNullable(member1));
+        given(userMemberRepository.findByNickName("userB")).willReturn(
+                Optional.ofNullable(member2));
 
-        Friend friend1 = friendRepository.findByUserMemberAndFriendId(member1, member2.getId()).get();
-        Friend friend2 = friendRepository.findByUserMemberAndFriendId(member2, member1.getId()).get();
+        friendService.requestFriend(member1.getId(), "userB");
+        Friend friend1 = member1.getFriends().get(0);
+        Friend friend2 = member2.getFriends().get(0);
 
         assertThat(friend1.getFriendId()).isEqualTo(member2.getId());
         assertThat(friend1.getFriendsStatus()).isEqualTo(FriendStatus.REQUEST);
@@ -83,29 +108,47 @@ class FriendServiceTest {
 
     @Test
     void 친구추가_재요청() {
-        friendService.requestFriend(member1.getId(), "userB");
-        friendService.acceptFriend(member2.getId(), "userA");
-        friendService.deleteFriend(member2.getId(), "userA");
+        addFriend(member1, member2, FriendStatus.REJECT);
+        addFriend(member2, member1, FriendStatus.REJECT);
+
+        given(userMemberRepository.findById(userMemberId1)).willReturn(Optional.ofNullable(member1));
+        given(userMemberRepository.findByNickName("userB")).willReturn(Optional.ofNullable(member2));
+
+        given(friendRepository.existsByUserMemberAndFriendId(member1, member2.getId()))
+                .willReturn(true);
+        given(friendRepository.findByUserMemberAndFriendId(member1, member2.getId()))
+                .willReturn(Optional.ofNullable(member1.getFriends().get(0)));
+        given(friendRepository.findByUserMemberAndFriendId(member2, member1.getId()))
+                .willReturn(Optional.ofNullable(member2.getFriends().get(0)));
 
         friendService.requestFriend(member1.getId(), "userB");
 
-        Friend friend1 = friendRepository.findByUserMemberAndFriendId(member1, member2.getId()).get();
-        Friend friend2 = friendRepository.findByUserMemberAndFriendId(member2, member1.getId()).get();
+        Friend friend1 = member1.getFriends().get(0);
+        Friend friend2 = member2.getFriends().get(0);
 
         assertThat(friend1.getFriendId()).isEqualTo(member2.getId());
         assertThat(friend1.getFriendsStatus()).isEqualTo(FriendStatus.REQUEST);
-
         assertThat(friend2.getFriendId()).isEqualTo(member1.getId());
         assertThat(friend2.getFriendsStatus()).isEqualTo(FriendStatus.RESPONSE);
     }
 
+
+
     @Test
     void 친구요청_수락() {
-        friendService.requestFriend(member1.getId(), "userB");
+        addFriend(member1, member2, FriendStatus.REQUEST);
+        addFriend(member2, member1, FriendStatus.RESPONSE);
+
+        given(userMemberRepository.findById(userMemberId2)).willReturn(Optional.ofNullable(member2));
+        given(userMemberRepository.findByNickName("userA")).willReturn(Optional.ofNullable(member1));
+
+        given(friendRepository.findByUserMemberAndFriendId(member2, member1.getId())).willReturn(Optional.ofNullable(member2.getFriends().get(0)));
+        given(friendRepository.findByUserMemberAndFriendId(member1, member2.getId())).willReturn(Optional.ofNullable(member1.getFriends().get(0)));
+
         friendService.acceptFriend(member2.getId(), "userA");
 
-        Friend friend1 = friendRepository.findByUserMemberAndFriendId(member1, member2.getId()).get();
-        Friend friend2 = friendRepository.findByUserMemberAndFriendId(member2, member1.getId()).get();
+        Friend friend1 = member1.getFriends().get(0);
+        Friend friend2 = member2.getFriends().get(0);
 
         assertThat(friend1.getFriendId()).isEqualTo(member2.getId());
         assertThat(friend1.getFriendsStatus()).isEqualTo(FriendStatus.ACCEPT);
@@ -116,11 +159,19 @@ class FriendServiceTest {
 
     @Test
     void 친구요청_거절() {
-        friendService.requestFriend(member1.getId(), "userB");
+        addFriend(member1, member2, FriendStatus.REQUEST);
+        addFriend(member2, member1, FriendStatus.RESPONSE);
+
+        given(userMemberRepository.findById(userMemberId2)).willReturn(Optional.ofNullable(member2));
+        given(userMemberRepository.findByNickName("userA")).willReturn(Optional.ofNullable(member1));
+
+        given(friendRepository.findByUserMemberAndFriendId(member2, member1.getId())).willReturn(Optional.ofNullable(member2.getFriends().get(0)));
+        given(friendRepository.findByUserMemberAndFriendId(member1, member2.getId())).willReturn(Optional.ofNullable(member1.getFriends().get(0)));
+
         friendService.rejectFriend(member2.getId(), "userA");
 
-        Friend friend1 = friendRepository.findByUserMemberAndFriendId(member1, member2.getId()).get();
-        Friend friend2 = friendRepository.findByUserMemberAndFriendId(member2, member1.getId()).get();
+        Friend friend1 = member1.getFriends().get(0);
+        Friend friend2 = member2.getFriends().get(0);
 
         assertThat(friend1.getFriendId()).isEqualTo(member2.getId());
         assertThat(friend1.getFriendsStatus()).isEqualTo(FriendStatus.REJECT);
@@ -131,13 +182,19 @@ class FriendServiceTest {
 
     @Test
     void 친구요청_삭제() {
-        friendService.requestFriend(member1.getId(), "userB");
-        friendService.acceptFriend(member2.getId(), "userA");
+        addFriend(member1, member2, FriendStatus.ACCEPT);
+        addFriend(member2, member1, FriendStatus.ACCEPT);
 
-        friendService.deleteFriend(member1.getId(), "userB");
+        given(userMemberRepository.findById(userMemberId2)).willReturn(Optional.ofNullable(member2));
+        given(userMemberRepository.findByNickName("userA")).willReturn(Optional.ofNullable(member1));
 
-        Friend friend1 = friendRepository.findByUserMemberAndFriendId(member1, member2.getId()).get();
-        Friend friend2 = friendRepository.findByUserMemberAndFriendId(member2, member1.getId()).get();
+        given(friendRepository.findByUserMemberAndFriendId(member2, member1.getId())).willReturn(Optional.ofNullable(member2.getFriends().get(0)));
+        given(friendRepository.findByUserMemberAndFriendId(member1, member2.getId())).willReturn(Optional.ofNullable(member1.getFriends().get(0)));
+
+        friendService.deleteFriend(member2.getId(), "userA");
+
+        Friend friend1 = member1.getFriends().get(0);
+        Friend friend2 = member2.getFriends().get(0);
 
         assertThat(friend1.getFriendId()).isEqualTo(member2.getId());
         assertThat(friend1.getFriendsStatus()).isEqualTo(FriendStatus.DELETE);
@@ -148,21 +205,30 @@ class FriendServiceTest {
 
     @Test
     void 친구리스트_조회() {
-        friendService.requestFriend(member1.getId(), "userB");
-        friendService.requestFriend(member3.getId(), "userB");
+        addFriend(member1, member2, FriendStatus.ACCEPT);
+        addFriend(member1, member3, FriendStatus.ACCEPT);
+        addFriend(member2, member1, FriendStatus.ACCEPT);
+        addFriend(member3, member1, FriendStatus.ACCEPT);
 
-        friendService.acceptFriend(member2.getId(), "userA");
-        friendService.acceptFriend(member2.getId(), "userC");
+        given(userMemberRepository.findById(member1.getId())).willReturn(Optional.ofNullable(member1));
+        given(friendRepository.findAllByUserMember(member1)).willReturn(member1.getFriends());
+        given(userMemberRepository.findNicknamesByIdList(mapFriendsToIdList(member1.getFriends())))
+                .willReturn(Arrays.asList("userB", "userC"));
 
-        List<FriendResponseDto> friendsList = friendService.getFriendsList(member2.getId());
+        List<FriendResponseDto> friendsList = friendService.getFriendsList(member1.getId());
 
-        List<Long> ids = Arrays.asList(member1.getId(), member3.getId());
-        List<String> nicknames = Arrays.asList("userA", "userC");
-        int idx = 0;
-        for (FriendResponseDto friendResponseDto : friendsList) {
-            assertThat(friendResponseDto.getFriendId()).isEqualTo(ids.get(idx));
-            assertThat(friendResponseDto.getFriendStatus()).isEqualTo(FriendStatus.ACCEPT);
-            assertThat(friendResponseDto.getNickName()).isEqualTo(nicknames.get(idx++));
+        int idxForTest = 0;
+        List<String> nicknamesForTest = Arrays.asList("userB", "userC");
+        for (FriendResponseDto dto : friendsList) {
+            assertThat(dto.getNickName()).isEqualTo(nicknamesForTest.get(idxForTest++));
+            assertThat(dto.getFriendStatus()).isEqualTo(FriendStatus.ACCEPT);
         }
+    }
+
+    private List<Long> mapFriendsToIdList(List<Friend> friends) {
+        List<Long> idList = friends.stream()
+                .map(friend -> friend.getFriendId())
+                .collect(Collectors.toList());
+        return idList;
     }
 }
