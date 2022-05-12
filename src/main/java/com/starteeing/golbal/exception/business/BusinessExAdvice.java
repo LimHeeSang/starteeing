6225560643f5718e8f90058b265fc1ef0c3dad3c
heesang <dlagmltkd12@naver.com>
2022-05-13 05:53:34 +0900

    feat : business exception Advice 구현

---
+++ src/main/java/com/starteeing/golbal/exception/business/BusinessExAdvice.java
@@ -0,0 +1,25 @@
+package com.starteeing.golbal.exception.business;
+
+import com.starteeing.golbal.response.ResponseService;
+import com.starteeing.golbal.response.result.CommonResult;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestControllerAdvice
+public class BusinessExAdvice {
+
+    private final ResponseService responseService;
+
+    @ExceptionHandler(BusinessException.class)
+    protected ResponseEntity<CommonResult> handleBusinessException(BusinessException e) {
+        CommonResult errorResult = responseService.getErrorResult(e.getExceptionEnum());
+        return ResponseEntity
+                .status(e.getHttpStatus())
+                .body(errorResult);
+    }
+}
