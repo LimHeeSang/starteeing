45d3f7e79bd146455daf5bec06851a2cc05f81cb
heesang <dlagmltkd12@naver.com>
2022-05-15 12:34:56 +0900

    refactor : CommonExAdvice 리팩토링

--- src/main/java/com/starteeing/golbal/exception/common/CommonExAdvice.java
+++ src/main/java/com/starteeing/golbal/exception/common/CommonExAdvice.java
@@ -5,10 +5,9 @@ import com.starteeing.golbal.response.result.CommonResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.net.BindException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,9 +16,9 @@ public class CommonExAdvice {
 
     private final ResponseService responseService;
 
-    @ExceptionHandler(BindException.class)
-    protected ResponseEntity<CommonResult> handleBindException(BindException e) {
-        log.error("handleBindException", e);
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<CommonResult> handleBindException(HttpMessageNotReadableException e) {
+        log.error("JsonParseException", e);
         CommonResult errorResult = responseService.getErrorResult(CommonExEnum.INVALID_BINGING_VALUE);
         return ResponseEntity.badRequest().body(errorResult);
     }
