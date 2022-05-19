6028b2be616bf8b3da5cc57bfc5f2bf7feb2d6ba
heesang <dlagmltkd12@naver.com>
2022-05-19 23:59:26 +0900

    feat : findByEmail 구현

--- src/main/java/com/starteeing/domain/member/repository/MemberRepository.java
+++ src/main/java/com/starteeing/domain/member/repository/MemberRepository.java
@@ -1,9 +1,14 @@
 package com.starteeing.domain.member.repository;
 
 import com.starteeing.domain.member.entity.Member;
+import com.starteeing.domain.member.entity.UserMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByEmail(String email);
+
+    Optional<Member> findByEmail(String email);
 }
