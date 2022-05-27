c1a74ca6da9825bdede34c5bf401c3e5b1ddb3e8
heesang <dlagmltkd12@naver.com>
2022-05-27 23:00:57 +0900

    feat : MemberRepository findByEmailWithMemberRoles 구현

--- src/main/java/com/starteeing/domain/member/repository/MemberRepository.java
+++ src/main/java/com/starteeing/domain/member/repository/MemberRepository.java
@@ -3,7 +3,9 @@ package com.starteeing.domain.member.repository;
 import com.starteeing.domain.member.entity.Member;
 import com.starteeing.domain.member.entity.UserMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -11,4 +13,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
 
     Optional<Member> findByEmail(String email);
+
+    @Query("select m from Member m left join m.memberRoles")
+    Optional<Member> findByEmailWithMemberRoles(String email);
 }
