c4a3ace3017613bc771c332e433bead0b9bbf1bd
heesang <dlagmltkd12@naver.com>
2022-06-02 12:34:56 +0900

    feat : MemberRole enum에서 class로 뽑아냄

---
+++ src/main/java/com/starteeing/domain/member/entity/MemberRole.java
@@ -0,0 +1,32 @@
+package com.starteeing.domain.member.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EqualsAndHashCode(exclude = {"memberRoleEnum"})
+@Builder
+@Entity
+public class MemberRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MemberRole_id")
+    private Long id;
+
+    private MemberRoleEnum memberRoleEnum;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public MemberRole(MemberRoleEnum memberRoleEnum) {
+        this.memberRoleEnum = memberRoleEnum;
+    }
+
+    public MemberRoleEnum getMemberRoleEnum() {
+        return memberRoleEnum;
+    }
+}
