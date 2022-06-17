56d5965c30ef8d9bb28cada4396e9a7a9f3d165c
heesang <dlagmltkd12@naver.com>
2022-06-17 23:24:27 +0900

    refactor : spring security ex enum 추가

--- src/main/java/com/starteeing/golbal/exception/common/CommonExEnum.java
+++ src/main/java/com/starteeing/golbal/exception/common/CommonExEnum.java
@@ -10,7 +10,9 @@ public enum CommonExEnum implements ExceptionEnum {
     SUCCESS(HttpStatus.OK, "E0001", "정상 요청입니다."),
     FAIL(HttpStatus.BAD_REQUEST, "E0002", "잘못된 요청입니다."),
 
-    INVALID_BINGING_VALUE(HttpStatus.BAD_REQUEST, "E0003", "입력값 중 바인딩이 실패하였습니다.");
+    INVALID_BINGING_VALUE(HttpStatus.BAD_REQUEST, "E0003", "입력값 중 바인딩이 실패하였습니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "E0004", "인증에 실패했습니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "E0005", "해당 권한이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
