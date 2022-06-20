31bcffa94f3a69986a578551dd673bb53f157306
heesang <dlagmltkd12@naver.com>
2022-06-20 12:35:56 +0900

    refactor : MemberExEnum refresh token 관련 enum 추가

--- src/main/java/com/starteeing/domain/member/exception/MemberExEnum.java
+++ src/main/java/com/starteeing/domain/member/exception/MemberExEnum.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum MemberExEnum implements ExceptionEnum {
     ALREADY_EXIST_MEMBER(HttpStatus.BAD_REQUEST, "M0001", "이미 존재하는 회원입니다."),
-    NOT_EXIST_MEMBER(HttpStatus.BAD_REQUEST, "M0002", "존재하지 않는 회원입니다.");
+    NOT_EXIST_MEMBER(HttpStatus.BAD_REQUEST, "M0002", "존재하지 않는 회원입니다."),
+    NOT_VALID_TOKEN(HttpStatus.UNAUTHORIZED, "M0003", "유효하지 않은 토큰입니다."),
+    NOT_EXIST_TOKEN(HttpStatus.UNAUTHORIZED, "M0004", "토큰이 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
