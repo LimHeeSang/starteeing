a5b58ae67cb89052972d69b570cf7d466160f884
heesang <dlagmltkd12@naver.com>
2022-06-25 12:41:56 +0900

    refactor : CommonExAdvice handleBindException 리팩토링

--- src/main/java/com/starteeing/golbal/exception/common/CommonExAdvice.java
+++ src/main/java/com/starteeing/golbal/exception/common/CommonExAdvice.java
@@ -6,8 +6,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.net.BindException;
 
 @RequiredArgsConstructor
 @RestControllerAdvice
@@ -15,8 +18,8 @@ public class CommonExAdvice {
 
     private final ResponseService responseService;
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    protected ResponseEntity<CommonResult> handleBindException(HttpMessageNotReadableException e) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<CommonResult> handleBindException(MethodArgumentNotValidException e) {
         CommonResult errorResult = responseService.getErrorResult(CommonExEnum.INVALID_BINGING_VALUE);
         return ResponseEntity.badRequest().body(errorResult);
     }
