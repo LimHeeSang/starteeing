5a04753b7099f6b94020b0f08af8823ad0a75ff4
heesang <dlagmltkd12@naver.com>
2022-07-19 12:34:56 +0900

    feat : TeamController 구현

---
+++ src/main/java/com/starteeing/domain/team/controller/TeamController.java
@@ -0,0 +1,56 @@
+package com.starteeing.domain.team.controller;
+
+import com.starteeing.domain.team.dto.TeamCreateRequestDto;
+import com.starteeing.domain.team.dto.TeamListResponseDto;
+import com.starteeing.domain.team.service.TeamService;
+import com.starteeing.golbal.response.ResponseService;
+import com.starteeing.golbal.response.result.CommonResult;
+import com.starteeing.golbal.response.result.SingleResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class TeamController {
+
+    private final TeamService teamService;
+    private final ResponseService responseService;
+
+    /**
+     * 팀 목록 조회
+     */
+    @GetMapping("/team/{memberId}")
+    public ResponseEntity<SingleResult> getTeamList(@PathVariable Long memberId) {
+        TeamListResponseDto teamListResponseDto = teamService.getTeamList(memberId);
+        return ResponseEntity.ok(responseService.getSingleResult(teamListResponseDto));
+    }
+
+    /**
+     * 팀 생성
+     */
+    @PostMapping("/team/{memberId}")
+    public ResponseEntity<SingleResult> createTeam(@PathVariable Long memberId, @RequestBody TeamCreateRequestDto createRequestDto) {
+        Long teamId = teamService.createTeam(memberId, createRequestDto);
+        return ResponseEntity.ok(responseService.getSingleResult(teamId));
+    }
+
+    /**
+     * 팀 멤버 추가
+     */
+    @PostMapping("/team/{memberId}/{teamId}/{friendId}")
+    public ResponseEntity<CommonResult> addTeamMember(
+            @PathVariable Long memberId, @PathVariable Long teamId, @PathVariable Long friendId) {
+        teamService.addTeamMember(memberId, teamId, friendId);
+        return ResponseEntity.ok(responseService.getSuccessResult());
+    }
+
+    /**
+     * 팀 탈퇴하기
+     */
+    @PostMapping("/team/{memberId}/{teamId}")
+    public ResponseEntity<CommonResult> withDrawTeam(@PathVariable Long memberId, @PathVariable Long teamId) {
+        teamService.withDrawTeam(memberId, teamId);
+        return ResponseEntity.ok(responseService.getSuccessResult());
+    }
+}
