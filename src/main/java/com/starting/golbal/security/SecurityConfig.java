4fafbeec70ae0e72c1a01846fa1190409d32b387
heesang <dlagmltkd12@naver.com>
2022-08-01 18:19:08 +0900

    feat : FriendService exception 리팩토링

--- src/main/java/com/starting/golbal/security/SecurityConfig.java
+++ src/main/java/com/starting/golbal/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.starting.golbal.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.starting.golbal.oauth.CorsProperties;
 import com.starting.golbal.oauth.OAuth2AuthorizationRequestBasedOnCookieRepository;
 import com.starting.golbal.oauth.OAuth2UserServiceImpl;
 import com.starting.golbal.oauth.handler.OAuth2AuthenticationFailureHandler;
@@ -21,11 +20,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
@@ -45,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(corsConfigurationSource())
+        http.cors()
                 .and()
                 .csrf().disable()
                 .httpBasic().disable()
@@ -123,7 +120,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://13.209.146.204:8080"));
+        configuration.setAllowedOrigins(Collections.singletonList("http://13.209.146.204:8080"));
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
