646042e4a01cd659b6083179ca7e2bb87a507e83
heesang <dlagmltkd12@naver.com>
2022-08-04 00:30:57 +0900

    feat : cors 설정

--- src/main/java/com/starting/domain/member/controller/UserMemberController.java
+++ src/main/java/com/starting/domain/member/controller/UserMemberController.java
@@ -11,10 +11,7 @@ import com.starting.golbal.response.result.SingleResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -43,6 +40,7 @@ public class UserMemberController {
         return ResponseEntity.ok(responseService.getSingleResult(memberLoginResponseDto));
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/test")
     public ResponseEntity<CommonResult> test() {
         return ResponseEntity.ok(responseService.getSingleResult("test가 성공했습니다."));
