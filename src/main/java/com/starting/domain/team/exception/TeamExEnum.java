ca21680c393bf216d53c531e8b716176043a900f
heesang <dlagmltkd12@naver.com>
2022-08-05 12:34:56 +0900

    feat : TeamExEnum 추가

---
+++ src/main/java/com/starting/domain/team/exception/TeamExEnum.java
@@ -0,0 +1,16 @@
+package com.starting.domain.team.exception;
+
+import com.starting.golbal.exception.common.ExceptionEnum;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum TeamExEnum implements ExceptionEnum {
+    NOT_EXIST_TEAM(HttpStatus.BAD_REQUEST, "T0001", "존재하지 않는 팀 입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
