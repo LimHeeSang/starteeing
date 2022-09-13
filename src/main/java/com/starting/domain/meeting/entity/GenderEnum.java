e6d72515266e9e2c440bf65e6548de56466f39f2
heesang <dlagmltkd12@naver.com>
2022-09-13 23:33:45 +0900

    feat : GenderEnum 메소드 추가

--- src/main/java/com/starting/domain/meeting/entity/GenderEnum.java
+++ src/main/java/com/starting/domain/meeting/entity/GenderEnum.java
@@ -7,4 +7,12 @@ public enum GenderEnum {
     MALE("M"), FEMALE("F");
 
     private final String genderType;
+
+    public boolean isMale() {
+        return genderType.equals(MALE.genderType);
+    }
+
+    public boolean isFemale() {
+        return genderType.equals(FEMALE.genderType);
+    }
 }
