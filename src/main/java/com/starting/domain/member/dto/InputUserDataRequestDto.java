dd7ab7bb1fc8334e6a385646d52cbcc6ac0bf880
heesang <dlagmltkd12@naver.com>
2022-09-18 16:05:12 +0900

    feat : InputUserDataRequestDto 구현

---
+++ src/main/java/com/starting/domain/member/dto/InputUserDataRequestDto.java
@@ -0,0 +1,27 @@
+package com.starting.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Builder
+@Getter
+public class InputUserDataRequestDto {
+
+    private final String name;
+
+    private final String birthOfDate;
+
+    private final String phoneNumber;
+
+    private final String nickname;
+
+    private final String mbti;
+
+    private final String school;
+
+    private final String department;
+
+    private final String uniqSchoolNumber;
+}
