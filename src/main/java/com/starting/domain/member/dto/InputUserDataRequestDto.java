2c396c85db10bbfab6371f8ca5a4bf912c0e08fd
heesang <dlagmltkd12@naver.com>
2022-09-23 22:43:16 +0900

    feat : InputUserDataRequestDto getter 어노테이션 추가

--- src/main/java/com/starting/domain/member/dto/InputUserDataRequestDto.java
+++ src/main/java/com/starting/domain/member/dto/InputUserDataRequestDto.java
@@ -1,10 +1,8 @@
 package com.starting.domain.member.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Builder
 @Getter
 public class InputUserDataRequestDto {
