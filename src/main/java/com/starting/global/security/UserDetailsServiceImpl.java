f22cc3c7a6831fe7f4fab2fc2e09f0cb4c5b76d4
heesang <dlagmltkd12@naver.com>
2022-09-23 21:14:51 +0900

    fix : UserDetailsServiceImpl loadUserByUsername 메소드 Oauth 로그인으로도 토큰 동작하도록 수정

--- src/main/java/com/starting/global/security/UserDetailsServiceImpl.java
+++ src/main/java/com/starting/global/security/UserDetailsServiceImpl.java
@@ -14,26 +14,42 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    private static final String DEFAULT_PASSWORD_OF_OAUTH2 = "";
+    public static final String EMAIL_REGEX = "^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-z]+$";
+
     private final MemberRepository memberRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        //임시 로직
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (isEmailRegex(username)) {
+            Member emailRegisterMember = memberRepository.findByEmailWithMemberRoles(username).orElseThrow(NotExistMemberException::new);
+            return createUserForEmail(emailRegisterMember);
+        }
 
-
-
-        Member member = memberRepository.findByEmailWithMemberRoles(email).orElseThrow(NotExistMemberException::new);
-        return createUser(member);
+        Member oAuthRegisterMember = memberRepository.findByUserIdWithMemberRoles(username).orElseThrow(NotExistMemberException::new);
+        return createUserForOauth(oAuthRegisterMember);
     }
 
-    private User createUser(Member member) {
-        return new User(member.getEmail(), member.getPassword(), mapToAuthorities(member.mapToMemberRoleEnum()));
+    private boolean isEmailRegex(String userName) {
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        Matcher matcher = pattern.matcher(userName);
+        return matcher.matches();
+    }
+
+    private User createUserForOauth(Member member) {
+        return new User(member.getUserId(), DEFAULT_PASSWORD_OF_OAUTH2, mapToAuthorities(member.mapToMemberRoleEnum()));
+    }
+
+    private User createUserForEmail(Member member) {
+        return new User(member.getUserId(), member.getPassword(), mapToAuthorities(member.mapToMemberRoleEnum()));
     }
 
     private List<GrantedAuthority> mapToAuthorities(List<MemberRoleEnum> memberRoleEnums) {
