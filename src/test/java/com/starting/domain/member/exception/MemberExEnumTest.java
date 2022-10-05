0ff03b0e16380abb9477853bcff764da580cdca0
heesang <dlagmltkd12@naver.com>
2022-10-05 17:56:41 +0900

    test : MemberExEnumTest 테스트 추가

--- src/test/java/com/starting/domain/member/exception/MemberExEnumTest.java
+++ src/test/java/com/starting/domain/member/exception/MemberExEnumTest.java
@@ -45,4 +45,20 @@ class MemberExEnumTest {
         assertThat(memberExEnum.getCode()).isEqualTo("M0005");
         assertThat(memberExEnum.getMessage()).isEqualTo("패스워드가 일치하지 않습니다.");
     }
+
+    @Test
+    void already_exist_nickname() {
+        MemberExEnum memberExEnum = MemberExEnum.ALREADY_EXIST_NICKNAME;
+
+        assertThat(memberExEnum.getCode()).isEqualTo("M0006");
+        assertThat(memberExEnum.getMessage()).isEqualTo("해당 닉네임이 이미 존재합니다.");
+    }
+
+    @Test
+    void already_input_userdata() {
+        MemberExEnum memberExEnum = MemberExEnum.ALREADY_INPUT_USERDATA;
+
+        assertThat(memberExEnum.getCode()).isEqualTo("M0007");
+        assertThat(memberExEnum.getMessage()).isEqualTo("이미 유저 정보를 입력했습니다.");
+    }
 }
