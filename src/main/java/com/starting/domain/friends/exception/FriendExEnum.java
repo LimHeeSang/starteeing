816ec708065b1a00d7cb0e59301b6196376ddccc
heesang <dlagmltkd12@naver.com>
2022-10-14 16:24:51 +0900

    feat : FriendExEnum 필드 추가

--- src/main/java/com/starting/domain/friends/exception/FriendExEnum.java
+++ src/main/java/com/starting/domain/friends/exception/FriendExEnum.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum FriendExEnum implements ExceptionEnum {
     NOT_EXIST_FRIEND(HttpStatus.BAD_REQUEST, "F0001", "찾으려는 친구가 없습니다."),
-    NOT_EXIST_PRE_REQUEST(HttpStatus.BAD_REQUEST, "F0002", "이전 요청이 없습니다.");
+    NOT_EXIST_PREVIOUS_REQUEST(HttpStatus.BAD_REQUEST, "F0002", "이전 친구 요청이 없습니다."),
+    ALREADY_REQUEST_FRIEND(HttpStatus.BAD_REQUEST, "F0003", "이미 친구요청을 보낸 회원입니다."),
+    ALREADY_FRIEND(HttpStatus.BAD_REQUEST, "F0004", "이미 친구인 회원입니다."),
+    ALREADY_RESPONSE_FRIEND(HttpStatus.BAD_REQUEST, "F0005", "이미 친구요청을 받은 회원입니다. 친구수락목록을 확인하세요");
 
     private final HttpStatus httpStatus;
     private final String code;
